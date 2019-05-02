5c7ffe7cf0bd680bdf86157bbd9c4a4e5cd6320d
Bethany <bethany.grogg@gmail.com>
2019-05-01 21:23:27 -0400

    Added some style

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         for (i = 0; i < shows.length; i++) {
             var showName = $("<button>");
             // Adding a class
-            showName.addClass("show");
+            showName.addClass("btn btn-success show");
             // Added a data-attribute
             showName.attr("data-name", shows[i]);
             // Provided the initial button text
@@ -19,60 +19,57 @@ $(document).ready(function () {
             $("#buttons-view").append(showName);
         }
     }
-    
-    $(document.body).on("click", ".show", function() {
 
-           
-                var tvShow = $(this).attr("data-name");
+    $(document.body).on("click", ".show", function () {
+
+        var tvShow = $(this).attr("data-name");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            tvShow + "&api_key=21pbXpSdx68vgJpuoB7wb0uQgVGGuGUg&limit=10";
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(function (response) {
+                var results = response.data;
                 
-                console.log(tvShow);
-                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-                    tvShow + "&api_key=21pbXpSdx68vgJpuoB7wb0uQgVGGuGUg&limit=10";
+                for (var i = 0; i < results.length; i++) {
+                    
+                    var gifDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var showImage = $("<img>");
+                    showImage.attr("src", results[i].images.fixed_height.url);
 
-                $.ajax({
-                        url: queryURL,
-                        method: "GET"
-                    })
-                    .then(function (response) {
-                        var results = response.data;
-console.log(results);
-                        for (var i = 0; i < results.length; i++) {
-                            var gifDiv = $("<div>");
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(showImage);
 
-                            var rating = results[i].rating;
+                    $("#gifs-appear-here").prepend(gifDiv);
+                }
+            });
 
-                            var p = $("<p>").text("Rating: " + rating);
-
-                            var showImage = $("<img>");
-                            showImage.attr("src", results[i].images.fixed_height.url);
-
-                            gifDiv.prepend(p);
-                            gifDiv.prepend(showImage);
-
-                            $("#gifs-appear-here").prepend(gifDiv);
-                        }
-                    });
-        
     });
 
 
 
 
-// This function handles events where the add tv show button is clicked
-$("#add-show").on("click", function (event) {
-    // event.preventDefault() prevents submit button from trying to send a form.
-    // Using a submit button instead of a regular button allows the user to hit
-    // "Enter" instead of clicking the button if desired
-    event.preventDefault();
+    // This function handles events where the add tv show button is clicked
+    $("#add-show").on("click", function (event) {
+        // event.preventDefault() prevents submit button from trying to send a form.
+        // Using a submit button instead of a regular button allows the user to hit
+        // "Enter" instead of clicking the button if desired
+        event.preventDefault();
 
-    // Write code to grab the text the user types into the input field
-    var userShow = $('#tv-input').val();
-    shows.push(userShow);
+        // grabs the text the user types into the input field
+        var userShow = $('#tv-input').val();
+        shows.push(userShow);
+        
+        renderButtons();
+        //clears the textbox after the user clicks the add show button
+        $('#tv-input').val("");
+    });
 
+    // Calling the renderButtons function to display the initial list of tv shows
     renderButtons();
-    $('#tv-input').val("");
-});
-
-// Calling the renderButtons function to display the initial list of tv shows
-renderButtons();
 });
